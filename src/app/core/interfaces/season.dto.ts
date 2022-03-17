852e4112290265004a247552613196da67d1d19c
Nikita Perepelitsa <nikitaperepel@gmail.com>
2022-03-17 11:10:41 +0100

    small formatting fix

--- src/app/core/interfaces/season.dto.ts
+++ src/app/core/interfaces/season.dto.ts
@@ -4,10 +4,11 @@ export interface SeasonDto {
   season: string;
   round: string;
   DriverStandings: {
-    wins: string; positionText: string;
+    wins: string;
+    positionText: string;
     Driver: DriverDto;
     Constructors: { nationality: string; name: string; constructorId: string; url: string }[];
     position: string;
-    points: string
+    points: string;
   }[];
 }
